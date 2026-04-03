e66e1590f27ef82a9a56878b8bd99999785de0bb
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2026-04-03 17:15:15 -0300

    scrolling background

---
+++ gd/scenes/parallax_2d.gd
@@ -0,0 +1,18 @@
+extends Parallax2D
+
+@export var speed: float = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+#	var movement := Vector2.ZERO
+#	movement.x += speed * delta
+#
+#	position += movement
+#
+	pass
