9cedc235eb4832a8c16efef762b775a2e1deaa40
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2026-04-07 22:31:26 -0300

    working zig build

--- Session.vim
+++ Session.vim
@@ -3,7 +3,7 @@ let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-
 let v:this_session=expand("<sfile>:p")
 silent only
 silent tabonly
-cd ~/Projects/gamedev/fearful_stars
+cd ~/projects/gamedev/fearful_stars
 if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
@@ -13,27 +13,68 @@ if &shortmess =~ 'A'
 else
   set shortmess=aoO
 endif
-badd +4 src/main.cpp
+badd +48 build.zig.zon
+badd +62 build.zig
+badd +84 src/gameplay/player/player.hpp
+badd +1202 term://~/projects/gamedev/fearful_stars//27133:/bin/bash
+badd +150 src/main.cpp
+badd +269 ~/.zvm/0.15.2/lib/libcxx/include/__memory/unique_ptr.h
+badd +3 src/engine/asset_man.hpp
+badd +7 src/deps.hpp
+badd +1858 ~/.zvm/0.15.2/lib/libcxx/include/algorithm
+badd +26 ~/.zvm/0.15.2/lib/libcxx/include/__algorithm/inplace_merge.h
+badd +15 src/draw_loop.hpp
+badd +224 term://~/projects/gamedev/fearful_stars//2056:/bin/bash
 argglobal
 %argdel
-$argadd oil:///home/huevo/Projects/gamedev/fearful_stars/
+$argadd .
+set stal=2
+tabnew +setlocal\ bufhidden=wipe
+tabrewind
 edit src/main.cpp
 argglobal
+balt src/gameplay/player/player.hpp
 setlocal foldmethod=indent
-setlocal foldexpr=0
+setlocal foldexpr=v:lua.vim.treesitter.foldexpr()
 setlocal foldmarker={{{,}}}
 setlocal foldignore=#
-setlocal foldlevel=10
+setlocal foldlevel=99
 setlocal foldminlines=1
 setlocal foldnestmax=20
 setlocal foldenable
-let s:l = 4 - ((3 * winheight(0) + 21) / 43)
+69
+sil! normal! zo
+116
+sil! normal! zo
+let s:l = 150 - ((29 * winheight(0) + 21) / 42)
 if s:l < 1 | let s:l = 1 | endif
 keepjumps exe s:l
 normal! zt
-keepjumps 4
+keepjumps 150
 normal! 0
+tabnext
+argglobal
+if bufexists(fnamemodify("term://~/projects/gamedev/fearful_stars//27133:/bin/bash", ":p")) | buffer term://~/projects/gamedev/fearful_stars//27133:/bin/bash | else | edit term://~/projects/gamedev/fearful_stars//27133:/bin/bash | endif
+if &buftype ==# 'terminal'
+  silent file term://~/projects/gamedev/fearful_stars//27133:/bin/bash
+endif
+balt build.zig
+setlocal foldmethod=indent
+setlocal foldexpr=v:lua.vim.treesitter.foldexpr()
+setlocal foldmarker={{{,}}}
+setlocal foldignore=#
+setlocal foldlevel=99
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldenable
+let s:l = 1202 - ((41 * winheight(0) + 21) / 42)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 1202
+normal! 02|
 tabnext 1
+set stal=1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
   silent exe 'bwipe ' . s:wipebuf
 endif
@@ -45,6 +86,8 @@ if filereadable(s:sx)
   exe "source " . fnameescape(s:sx)
 endif
 let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
 doautoall SessionLoadPost
 unlet SessionLoad
 " vim: set ft=vim :
--- build.zig
+++ build.zig
@@ -2,22 +2,40 @@ const std = @import("std");
 
 const cpp_source = [_][]const u8 {
 
-    "src/engine/debugger.cpp",
-    "src/engine/asset_man.cpp",
-    "src/engine/input_man.cpp",
-    "src/engine/music_man.cpp",
+
     "src/engine/background_man.cpp",
+    "src/engine/input_man.cpp",
     "src/engine/render_man.cpp",
+    "src/engine/asset_man.cpp",
+    "src/engine/music_man.cpp",
+    "src/engine/debugger.cpp",
+    "src/systems.cpp",
+    "src/gameplay/enemy/vagant/vagant.cpp",
+    "src/gameplay/enemy/chaser/chaser.cpp",
+    "src/gameplay/enemy/eye/crystal_eye.cpp",
     "src/gameplay/enemy/basic/basic_enemy.cpp",
     "src/gameplay/enemy/enemy_man.cpp",
-    "src/gameplay/levels/levels.cpp",
-    "src/gameplay/player/primary_shots/basic_shot.cpp",
-    "src/gameplay/player/player.cpp",
-    "src/gameplay/player/player_manager.cpp",
+    "src/gameplay/enemy/minion/minion.cpp",
+    "src/gameplay/enemy/anemonae/anemonae.cpp",
+    "src/gameplay/enemy/broken_ship/broken_ship.cpp",
+    "src/gameplay/projectile/orbital/orbital_proj.cpp",
+    "src/gameplay/projectile/missile/missile_proj.cpp",
+    "src/gameplay/projectile/plasma/plasma.cpp",
     "src/gameplay/projectile/basic/basic_projectile.cpp",
+    "src/gameplay/projectile/upgrade/upgrade_proj.cpp",
     "src/gameplay/projectile/projectile_manager.cpp",
-    "src/systems.cpp",
+    "src/gameplay/projectile/big_shot/bigshot_proj.cpp",
+    "src/gameplay/player/aux_powers/orbital_shield.cpp",
+    "src/gameplay/player/primary_shots/basic_shot.cpp",
+    "src/gameplay/player/primary_shots/plasma_shot.cpp",
+    "src/gameplay/player/player.cpp",
+    "src/gameplay/player/special_shots/big_shooter.cpp",
+    "src/gameplay/player/player_manager.cpp",
+    "src/gameplay/player/secondary_shots/missile_shooter.cpp",
+    "src/gameplay/ui/ui_man.cpp",
+    "src/gameplay/levels/levels.cpp",
     "src/main.cpp",
+
 
     "dependencies/dear_imgui/imgui.cpp",
     "dependencies/dear_imgui/imgui_demo.cpp",
@@ -38,31 +56,38 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib_dep = b.dependency("raylib", .{});
+    const raylib_dep = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+        .linux_display_backend = .X11
+    });
 
     const raylib_lib = raylib_dep.artifact("raylib");
 
-    const exe = b.addExecutable(.{
-        .name = "fearful_stars",
-        .root_module = b.addModule("main", .{
-            .optimize = optimize,
-            .target = target,
-            .link_libcpp = true,
-        }),
+    var fs_mod = b.addModule("game", .{
 
+        .link_libc = true,
+        .link_libcpp = true,
+        .target = target,
+        .optimize = optimize,
     });
 
-    exe.addCSourceFiles(.{
+    fs_mod.addCSourceFiles(.{
         .files = cpp_source[0..],
         .flags = cpp_flags[0..],
     });
 
-    exe.linkLibrary(raylib_lib);
+    fs_mod.linkLibrary(raylib_lib);
 
-    exe.addIncludePath(b.path("src/engine/"));
-    exe.addIncludePath(b.path("include/"));
-    exe.addIncludePath(b.path("src/"));
-    exe.addIncludePath(b.path("dependencies/dear_imgui/"));
+    fs_mod.addIncludePath(b.path("src/engine/"));
+    fs_mod.addIncludePath(b.path("include/"));
+    fs_mod.addIncludePath(b.path("src/"));
+    fs_mod.addIncludePath(b.path("dependencies/dear_imgui/"));
+
+    const exe = b.addExecutable(.{
+        .name = "fearful_stars",
+        .root_module = fs_mod,
+    });
 
 
     const assets = b.addInstallDirectory(.{
